94eee43d85a1383a3fce5896d6b484c7046955b3
amirulmenjeni <amirulmenjeni@gmail.com>
2017-10-10 14:16:40 +0800

    Add redditor component to reddit spider.

--- src/spiders/reddit_spider.py
+++ src/spiders/reddit_spider.py
@@ -11,7 +11,9 @@ class RedditSpider(Spider):
     r = None # Reddit praw instance.
     name = 'reddit'
 
-    def __type_list(value):
+    def comma_separated_list(value):
+        if value is None:
+            return value
         return [v.strip() for v in value.split(',')]
 
     def setup_filter(self, sf):
@@ -20,9 +22,9 @@ class RedditSpider(Spider):
         """
 
         # Create components.
-        sf.add_com('post', info='A user post or submission')
-        sf.add_com('comment', info='A user comment with respect to a post.')
-        sf.add_com('redditor', info='A redditor')
+        sf.add_com('post', info='A user submission (also called as post).')
+        sf.add_com('comment', info='A user comment reply to a submission.')
+        sf.add_com('redditor', info='A redditor (also known as neckbeards).')
 
         # Add post attributes.
         sf_post = sf.get('post')
@@ -42,21 +44,38 @@ class RedditSpider(Spider):
         # Add redditor attributes.
         sf_redditor = sf.get('redditor')
         sf_redditor.add('username', info='The redditor\'s username.')
-        sf_redditor.add('trophies')
+        sf_redditor.add('trophies', info='Trophies owned by the redditor.')
 
         # Create variables.
-        sf.add_var('subreddits', 
-                   default='all', type=RedditSpider.__type_list,
-                   info='The list of subreddits to scan, seperated by comma.')
-        sf.add_var('sections', default='hot, rising, new, top',
-                    info='Get submissions that only presents in this list.')
-        sf.add_var('skip_comments', default=False, type=bool,
-                   info='Skip comments for each scanned post if set to True.')
-        sf.add_var('redditor', default=None, type=RedditSpider.__type_list,
-                   info='A list of redditor usernames separated by comma. '\
-                        'If there\'s at least one redditor set in this list, '\
-                        'the spider will scan the redditor(s) instead of '\
-                        'subreddit.')
+        sf.add_var(
+            'subreddits',  default='all', 
+            type=RedditSpider.comma_separated_list,
+            info='The list of subreddits to scan, seperated by comma.'
+        )
+        sf.add_var(
+            'sections', default='hot, rising, new, top',
+            info='The section(s) where you want the spider to scan.'
+        )
+        sf.add_var(
+            'skip_comments', default=False, type=bool,
+            info='If this is set to True, comments will not be scanned '\
+                 'for each scanned submission (post component).'
+        )
+        sf.add_var(
+            'skip_redditors', default=True, type=bool,
+            info='If this is set to True, redditors (redditor component) '\
+                 'will not be scanned for each scanned '\
+                 'comment (comment component) '
+        )
+        sf.add_var(
+            'redditors', default=None, type=RedditSpider.comma_separated_list,
+            info='A list of redditor usernames separated by comma. '\
+                 'If there\'s at least one redditor set in this list, '\
+                 'the spider will scan the redditor(s) instead of '\
+                 'subreddit. Additionally, the @sections argument '\
+                 'will be used to select the section(s) to scan the '\
+                 'redditors\' submissions instead.'
+        )
 
     def start(self, sf):
         """
@@ -94,50 +113,130 @@ class RedditSpider(Spider):
 
         # Get the sections from which the post appear in each
         # subreddit.
-        sections = self.get_sections(sf.ret('sections'), sf.ret('subreddits'))
+        sub_sections = self.get_subreddit_sections(
+            sf.ret('sections'), sf.ret('subreddits')
+        )
 
+        # Get the sections from which the post/comment appear
+        # in each redditor page.
+        usr_sections = self.get_redditor_sections(
+            sf.ret('sections'), sf.ret('redditors')
+        )
+        
+        if not sf.ret('redditors'):
+            for section in self.scrape_subreddits_sections(sf, sub_sections):
+                yield section
+        else:
+            for redditor in self.scrape_redditors_sections(sf, usr_sections):
+                yield redditor
+
+    def scrape_subreddits_sections(self, sf, sections):
+        """
+        @param sf: ScrapeFilter of this spider.
+        @param sections: A listing generator.
+
+        Scrape each selected section(s) in the selected subreddit(s),
+        given by the sections (generator) object.
+        """
         for section in sections:
-            for post in section:
-                # Assign each component's attribute.
-                sf_post.set_attr_values(
-                    title=post.title,
-                    score=int(post.score),
-                    subreddit=str(post.subreddit),
-                    author=str(post.author)
-                )
+            for submission in self.scrape_submissions(sf, section):
+                yield submission
 
-                # Scrape the post if it pass the filter.
-                if sf_post.should_scrape():
-                    yield ComponentLoader('post', {
-                        'post_id': post.id,
-                        'title': post.title,
-                        'subreddit': str(post.subreddit),
-                        'score': post.score,
-                        'author': str(post.author)
-                    })
+    def scrape_submissions(self, sf, section):
+        """
+        @param sf: ScrapeFilter of this spider.
+        @param section: A section object which contains post objects.
 
-                if sf.ret('skip_comments'):
-                    continue
+        Scrape each post from the given section.
+        """
+        sf_post = sf.get('post')
+        for post in section:
+            # Assign each component's attribute.
+            sf_post.set_attr_values(
+                title=post.title,
+                score=int(post.score),
+                subreddit=str(post.subreddit),
+                author=str(post.author)
+            )
 
-                # Scraping (most if not all) comments of each post.
-                post.comments.replace_more(limit=0)
-                for comment in post.comments.list():
-                    sf_comment.set_attr_values(
-                        body=comment.body,
-                        score=comment.score,
-                        author=str(comment.author)
+            # Scrape the post if it pass the filter.
+            if sf_post.should_scrape():
+                yield ComponentLoader('post', {
+                    'post_id': post.id,
+                    'title': post.title,
+                    'subreddit': str(post.subreddit),
+                    'score': post.score,
+                    'author': str(post.author)
+                })
+
+                if not sf.ret('skip_comments'):
+                    for comment in self.scrape_comments(sf, post):
+                        yield comment
+
+    def scrape_comments(self, sf, post):
+        """
+        @param sf: ScrapeFilter of this spider.
+        @param post: A post object which contains comment objects.
+        """
+        sf_comment = sf.get('comment')
+        # Scraping (most if not all) comments of each post.
+        post.comments.replace_more(limit=0)
+        for comment in post.comments.list():
+            sf_comment.set_attr_values(
+                body=comment.body,
+                score=comment.score,
+                author=str(comment.author)
+            )
+
+            if sf_comment.should_scrape():
+                yield ComponentLoader('comment', {
+                    'comment_id': comment.id,
+                    'author': str(comment.author),
+                    'body': comment.body,
+                    'score': str(comment.score)
+                })
+
+                if not sf.ret('skip_redditors'):
+                    usr_sections = self.get_redditor_sections(
+                        sf.ret('sections'), [str(comment.author)]
                     )
+                    for redditor in self.scrape_redditors_sections(
+                        sf, usr_sections
+                    ):
+                        yield redditor 
 
-                    if sf_comment.should_scrape():
-                        yield ComponentLoader('comment', {
-                            'comment_id': comment.id,
-                            'author': str(comment.author),
-                            'body': comment.body,
-                            'score': str(comment.score)
-                        })
+    def scrape_redditors_sections(self, sf, sections):
+        for section in sections:
+            for item in self.scrape_redditors(sf, section):
+                yield item
 
-    # Get which section(s) to scrape the submissions from.
-    def get_sections(self, sections, scan_subs):
+    def scrape_redditors(self, sf, section):
+        for item in section:
+            if isinstance(item, praw.models.Comment):
+                yield ComponentLoader('comment', {
+                    'comment_id': item.id,
+                    'author': str(item.author),
+                    'body': item.body,
+                    'score': str(item.score)
+                })
+            elif isinstance(item, praw.models.Submission):
+                yield ComponentLoader('post', {
+                    'post_id': item.id,
+                    'title': item.title,
+                    'subreddit': str(item.subreddit),
+                    'score': item.score,
+                    'author': str(item.author)
+                })
+
+    def get_subreddit_sections(self, sections, scan_subs):
+        """
+        @param sections: The list of section(s) to select from.
+        @param scan_subs: The list of subreddit(s) to scan.
+
+        Get which section(s) in which subreddit(s) to scrape
+        the submissions from. This method returns generator
+        object of selected sections.
+        """
         scan_subs = '+'.join(scan_subs)
 
         # Chain the listing generators of each section
@@ -155,3 +254,24 @@ class RedditSpider(Spider):
         if 'top' in sections:
             top_section = self.r.subreddit(scan_subs).top(limit=None)
             yield top_section
+
+    def get_redditor_sections(self, sections, scan_redditors):
+        """
+        @param sections: The list of section(s) to choose from.
+        @param scan_redditors: The list of redditor(s) to scan.
+
+        Get which section(s) in which redditor(s) to scrape
+        the submissions and comments from. This method
+        returns generator object of selected sections.
+        """
+
+        for username in scan_redditors:
+            if 'hot' in sections:
+                hot_section = self.r.redditor(username).hot()
+                yield hot_section
+            if 'new' in sections:
+                new_section = self.r.redditor(username).new()
+                yield new_section
+            if 'top' in sections:
+                top_section = self.r.redditor(username).top()
+                yield top_section
