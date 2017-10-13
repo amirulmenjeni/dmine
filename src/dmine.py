ca1bb7c6c63e43451b7a3888a706393ca0f6893c
amirulmenjeni <amirulmenjeni@gmail.com>
2017-10-13 22:18:57 +0800

    Fix minor issue.

--- src/dmine.py
+++ src/dmine.py
@@ -88,7 +88,9 @@ class Component:
             self.get(k).is_assigned = True
 
         if not lenient:
-            for k in attributes:
+            for k in self.attr:
+                print(k, self.get(k).is_assigned)
+                input()
                 if not self.get(k).is_assigned:
                     self.__throw_value_unassigned_error(k)
                     break
