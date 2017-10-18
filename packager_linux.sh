212cf949464c7469d8c82980de2adbad6c9ec8fa
amirulmenjeni <amirulmenjeni@gmail.com>
2017-10-18 18:11:54 +0800

    Add a bunch of files for linux installation procedure.

--- packager_linux.sh
+++ packager_linux.sh
@@ -1,3 +1,14 @@
+function check_file {
+    if [ ! -f $1 ]; then
+        echo "Error: $1 is required for packaging, but it doesn't exist." 1>&2
+        exit 1
+    fi
+}
+
+check_file "build_linux.sh"
+check_file "installer_linux.sh"
+bash -c ./build_linux.sh
+
 echo "Run install.sh to install dmine." > readme.txt
 cp ./installer_linux.sh ./install.sh
 zip -r dmine_linux.zip ./builds/linux/dmine install.sh readme.txt
