cba9decffbf62ec11f6239385174f456e65cea0e
nz-is <31930922+nz-is@users.noreply.github.com>
2017-10-22 16:32:52 +0800

    xx

--- src/spiders/tweet_spider.py
+++ src/spiders/tweet_spider.py
@@ -4,47 +4,15 @@ import re
 import os
 import platform
 import logging
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
+
 from dmine import Spider, ComponentLoader
 
-#   Simple Twitter spider using Tweepy and Selenium
+#   Simple Twitter spider using Tweepy API
 #   You will need to generate your own API keys before accessing the Twitter api
 #   Guide on how this is done: https://auth0.com/docs/connections/social/twitter
 
 class TweetSpider(Spider):
     name = 'twitter'
-
-    def __init__(self):
-        self.driver= self.init_driver()
-
-    def init_driver(self):
-        path = os.path.relpath('../dep-bin/')
-
-        if platform.uname().system == 'Linux':
-            if platform.uname().machine == 'x86_64':
-                path = os.path.join(
-                    path, 'phantomjs-2.1.1-linux-x86_64', 'bin', 'phantomjs'
-                )
-            elif platform.uname().machine == 'i686':
-                path = os.path.join(
-                    path, 'phantomjs-2.1.1-linux-i686', 'bin', 'phantomjs'
-                )
-        elif platform.uname().system == 'Windows':
-            path = os.path.join(
-                path, 'phantomjs-2.1.1-windows', 'bin', 'phantomjs'
-            )
-        else:
-            msg = 'Unsupported platform: (%s, %s)'\
-                  % (platform.system(), platform.machine())
-            logging.error(msg)
-            raise NotImplemented(msg)
-
-        logging.info('Using phantomjs binary: %s', path)
-        driver = webdriver.PhantomJS(executable_path=path)
-        driver.wait = WebDriverWait(driver, 5)
-        return driver
 
     def setup_filter(self, sf):
         sf.add_com('tweet', info="A Tweet post")
@@ -56,12 +24,12 @@ class TweetSpider(Spider):
         sf_tweet.add('author', info='author of the tweet')
         sf_tweet.add('lang', info='The language of the tweet post')
         sf_tweet.add('retweet_count', info='The retweets of the tweet')
-        sf_tweet.add('likes_count',  info='The likes of the tweet')
+        sf_tweet.add('fav_count',  info='The likes of the tweet')
         sf_tweet.add('replies_count', info='Replies of the tweet')
 
         # Attributes on replies / comments [WIP]
         sf_replies = sf.get('replies')
-        sf_replies.add('retweets', info='The retweets of the comment')
+        sf_replies.add('retweet_count', info='The retweets of the comment')
         sf_replies.add('likes', info='The likes of the comment')
         sf_replies.add('body', info='The reply text body.')
         sf_replies.add('author', info='The user who posted')
@@ -121,12 +89,11 @@ class TweetSpider(Spider):
 
         if not keyword:
             trendings=self.fetch_trendings(api)
-            #for trend in trendings:
         for trend in trendings:
             keyword=trend
             while True:
                 try:
-                    new_tweets = api.search(q=keyword, lang=sf.ret('lang'), rrp=100, count=1500, result_type=sf.ret('tweet_type'), max_id=str(last_id - 1)) #max results per page
+                    new_tweets = api.search(q=keyword, lang=sf.ret('lang'), count=100, result_type=sf.ret('tweet_type'), max_id=str(last_id - 1)) #max results per page
                     if not new_tweets:
                         break
                     for tweet in self.scrape_tweet(api, sf, new_tweets):
@@ -135,6 +102,7 @@ class TweetSpider(Spider):
 
                 except tweepy.TweepError as e:
                     break
+
 
     def scrape_tweet(self, api, sf, searched_tweets):
         sf_tweet = sf.get('tweet')
@@ -154,7 +122,7 @@ class TweetSpider(Spider):
                      lang=x.lang,
                      retweet_count=x.retweet_count,
                      replies_count=replies_count,
-                     likes_count=fav_count
+                     fav_count=fav_count
             )
 
             if sf_tweet.should_scrape():
@@ -167,13 +135,53 @@ class TweetSpider(Spider):
                                                  'Date created' : x.created_at.strftime("%T %B %d, %Y"),
                                                  'retweet' : int(x.retweet_count),
                                                  'replies' : replies_count,
-                                                 'fav count' : int(fav_count)
+                                                 'fav_count' : int(fav_count)
                 })
 
             #yield replies if skip_replies set to False and replies count not 0
-            if not sf.ret('skip_replies') and replies_count != 0:
-                for rep in self.fetch_replies(self.driver, tag_name, tweet_id, sf):
-                    yield rep
+            if not sf.ret('skip_replies') and int(replies_count) > 0:
+                #self.mentions_replies( api, tag_name, tweet_id, sf, reply_count)
+                sf_replies=sf.get('replies')
+                max_id = None
+                while True:
+                    try:
+                        replies =api.search(q="to:%s" % tag_name, since_id=int(tweet_id), max_id=max_id,rpp=100, count=1500)
+                    except twitter.error.TwitterError as e:
+                        logging.error("caught twitter api error: %s", e)
+                        time.sleep(60)
+                        continue
+                    for reply in replies:
+                        print(":O")
+                        if reply.in_reply_to_status_id == tweet_id:
+
+                            counts=api.get_status(tweet_id)
+                            replies_count =counts.retweet_count
+                            fav_count =counts.favorite_count
+
+                            sf_replies.set_attr_values(
+                                     author= reply.user.screen_name,
+                                     retweet_count=reply.retweet_count,
+                                     likes=fav_count,
+                                     body=reply.text,
+                                     replies_count=replies_count
+                            )
+
+                            if sf_replies.should_scrape():
+                                yield ComponentLoader('replies', {'reply_id' :reply.id,
+                                                                  'author' : reply.user.screen_name,
+                                                                  'body' : reply.text,
+                                                                  'retweet_count' : reply.retweet_count,
+                                                                  'replies_count':replies_count,
+                                                                  'likes' : reply.favorite_count
+                                                                 })
+
+                            # recursive magic to also get the replies to this reply
+                            for reply_to_reply in get_replies(reply):
+                                yield reply_to_reply
+                                max_id = reply.id
+
+                    if len(replies) != 100:
+                        break
 
             if sf.ret('skip_author_info'):
                 continue
@@ -202,75 +210,21 @@ class TweetSpider(Spider):
                             'is_verified' : x.user.verified,
                         })
 
-    def fetch_replies(self, driver, author_name, tweet_id, sf):
-        sf_replies=sf.get('replies')
-        driver.get("https://twitter.com/{}/status/{}".format(author_name, tweet_id))
-        time.sleep(2)
-        try:
-            replies_div =driver.find_elements_by_xpath(".//div[@data-component-context='replies']")
-
-            #check if all replies are loaded
-            current_page_length = 0
-            while True:
-                replies_div[len(replies_div)-1].send_keys(Keys.PAGE_DOWN)
-                time.sleep(1)
-                prev = current_page_length
-                current_page_length = driver.execute_script("return document.body.scrollHeight")
-                if current_page_length == prev : #If page height is retained break loop // no more results
+    def mentions_replies(self, api, tag_name, tweet_id, sf, reply_count):
+        last_id = -1
+        i=0
+        flag = False
+        while True:
+            try:
+                new_tweets=api.search(q="to:"+tag_name, since_id=int(tweet_id), rpp=1, count=1, max_id=str(last_id - 1))
+                if not new_tweets:
                     break
+                for tweet in new_tweets:
+                    if tweet.in_reply_to_status_id_str == str(tweet_id):
+                        print("found a reply")
+                        i+=1
+                last_id = new_tweets[-1].id
 
-            replies_div =driver.find_elements_by_xpath("//div[@data-component-context='replies']")
-        except:
-            pass
-
-        c_list=[]
-        replies_div.pop(0)
-
-        #Scrape replies
-        for x in replies_div:
-            tweet_id = x.get_attribute("data-tweet-id")
-            contents = x.find_element_by_xpath(".//div[@class='js-tweet-text-container']")
-            author = x.find_element_by_class_name("stream-item-header").text
-            username=author.split(' ')[0]
-            tag_name=author.split(' ')[1]
-            date_created=x.find_element_by_xpath(".//a[@class='tweet-timestamp js-permalink js-nav js-tooltip']").get_attribute('title')
-            replies_response=x.find_elements_by_xpath(".//div[@class='ProfileTweet-actionList js-actions']/div")
-
-            reply_count= replies_response[0].text.split('\n')[0]
-            if not reply_count.isdigit() : reply_count = 0
-
-            retweet_count=replies_response[1].text.split('\n')
-            if len(retweet_count) > 1:
-                retweet_count = int(replies_response[1].text.split('\n')[1])
-            else:
-                retweet_count=0
-
-            likes_count=replies_response[2].text.split('\n')
-            if len(likes_count) == 2:
-                likes_count = likes_count[1]
-            else:
-                likes_count=0
-
-            #checks if img/gif div is present
-            if len(contents.text) == 0:
-                contents.find_element_by_xpath("//div[@class='AdaptiveMediaOuterContainer']")
-                c_list.append("user replied with a gif/img file")
-            else:
-                c_list.append(contents)
-
-            #set component here
-            sf_replies.set_attr_values(
-                    retweets= int(retweet_count),
-                    likes=int(likes_count),
-                    body = contents.text,
-                    author= tag_name
-            )
-
-            if sf_replies.should_scrape():
-                        yield ComponentLoader('replies', {
-                                'reply_id': int(tweet_id),
-                                'author' : tag_name,
-                                'body' : self.remove_emojis(contents.text),
-                                'retweets' : int(retweet_count),
-                                'fav_count' : int(likes_count)
-                        })
+            except tweepy.TweepError as e:
+                break
+        print(i, reply_count)
