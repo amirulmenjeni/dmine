aaa9c6ac7b57fb02d834e57f48ca46b44e113422
nz-is <31930922+nz-is@users.noreply.github.com>
2017-10-24 18:37:23 +0800

    Update installer_windows.bat

--- installer/windows/installer_windows.bat
+++ installer/windows/installer_windows.bat
@@ -1,11 +1,69 @@
 @echo off
-title=Dmine Installer
-rem Integrity check: dep-bin, dmine, schortcut.ch must be in one folder
-set expected_file= dep-bin dmine shortcut.ch
 
-(for %%a in (%expected_file%) do (
-  CALL :expect %%a
-))
+set dep-bin=dep-bin
+set build_path=Dmine
+set shortcut=shortcut.sh
+
+CALL :expect %dep-bin%
+CALL :expect %build_path%
+CALL :expect %shortcut%
+
+:: Checks if dmine folder exists in program files directory
+if EXIST "%ProgramFiles%\Dmine" (
+    echo "Warning: A directory called dmine already exists in ProgramFiles. Do you want to delete this existing directory to install dmine?"
+    goto validate_response
+
+    :validate_response
+    set /p reply="[Y | N]? "
+    if /I "%reply%"=="y" (
+       del /S /Q "%ProgramFiles%/Dmine\*"
+       goto unload_copy
+    )
+    if /I "%reply%"=="n" (
+       echo Aborting installation
+       ::goto end
+       EXIT /b 1
+    ) else (
+       echo "Please enter a valid response"
+       goto validate_response
+    )
+)
+
+echo "==> Installing to 'C:/Program Files/dmine'."
+
+MD "%ProgramFiles%/Dmine"
+
+:unload_copy
+xcopy /O /X /E /H /K  "%build_path%" "%ProgramFiles%\Dmine\%build_path%\"
+xcopy /O /X /E /H /K  "%dep-bin%" "%ProgramFiles%\Dmine\%dep-bin%\"
+
+if EXIST "%ProgramFiles%/%build_path%" (
+  echo "Build path Success."
+) else (
+  echo "Failed."
+  EXIT /b 0
+)
+
+:: Create shortcut script to the executable file at USERPROFILE/Desktop
+set shortcut_path="%USERPROFILE%\Desktop\Dmine_2"
+echo "==> Creating a shortcut to '%shortcut_path%'."
+
+if EXIST "%shortcut_path%" (
+    echo "Shortcut already exists. Removing it to create a new one."
+    del /Q /S "%shortcut_path%\*"
+)
+
+goto end
+
+MD "%shortcut_path%\Dmine"
+
+if EXIST "%shortcut_path%" (
+    echo "Success."
+    EXIT /b 1
+) else (
+    echo "Failed."
+    EXIT /b 0
+)
 
 :expect
 IF EXIST "%~1" (
@@ -15,47 +73,8 @@ IF EXIST "%~1" (
     EXIT /b 1
 )
 
-rem Checks if dmine folder exists in program files directory
-if EXIST "%ProgramFiles%\Dmine" (
-    echo "Warning: A directory called dmine already exists in ProgramFiles.
-    Do you want to delete this existing directory to install dmine?"
-
-       :validate_response
-       set /p reply="[y/n]? "
-       if /I "%reply%"=="y" (
-          del /S "%ProgramFiles%/Dmine\*"
-          goto unload_copy
-       )
-       if /I "%reply%"=="n" (
-          echo "Aborting installation."
-          set "reply="
-          EXIT /b 1
-       ) else (
-          echo "Please enter a valid response"
-          set "reply="
-          goto validate_response
-       )
-)
-
-echo "==> Installing to '/Program Files/dmine'."
-MD "%ProgramFiles%/Dmine"
-:unload_copy
-xcopy /S /E "Dmine" "%ProgramFiles%/Dmine"
-xcopy /S /E "dep-bin" "%ProgramFiles%/Dmine"
-
-if EXIST "%ProgramFiles%/Dmine" (
-  echo "Success."
-) else (
-  echo "Failed."
-  EXIT /b 0
-)
-:: Create shortcut script to the executable file at USERPROFILE/Desktop
-shortcut_path="%USERPROFILE%\Desktop"
-echo "==> Creating a shortcut to '%shortcut_path%'."
-
-
-
-
 :: Finish.
 echo "==> Installation finished. Please run 'dmine' to ensure successful "\
      "installation."
+
+:End
