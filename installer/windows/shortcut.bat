6f99fd176abc1fd3531b8be62f0c6766fdb15361
nz-is <31930922+nz-is@users.noreply.github.com>
2017-10-24 22:15:40 +0800

    Update shortcut.bat

--- installer/windows/shortcut.bat
+++ installer/windows/shortcut.bat
@@ -1,6 +1,5 @@
 @echo off
 
 set shortcut_path=%USERPROFILE%/.dmine
-cmd.exe /K %shortcut_path%/shortcut.bat
 DOSKEY dmine=%ProgramFiles%/Dmine/Dmine/Dmine.exe
 DOSKEY cd_dmine=cd %ProgramFiles%/Dmine/Dmine/ ::change directory to dmine build path
