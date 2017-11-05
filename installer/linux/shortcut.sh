8081aac589f08ece2b965496ce95ae03638b5ce6
amirulmenjeni <amirulmenjeni@gmail.com>
2017-11-06 00:35:52 +0800

    Update linux's shortcut script.

--- installer/linux/shortcut.sh
+++ installer/linux/shortcut.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+
+# This shortcut will create a temporary sym link
+# to the dmine executable binary to allow user to run dmine while
+# having access to its binary dependencies.
+
+# Check if a file or dir named 'dmine' exists.
+# If it does, then rename the file/dir temporarily.
+if [ -f dmine ] || [ -d dmine ]; then
+    mv dmine .__temp_dmine__
+fi
+
+# Create a temporary sym link.
 ln -s /opt/dmine/dmine/dmine ./dmine
 ./dmine "$@"
-if [ -L ./dmine ]; then
-    rm ./dmine
+
+# Remove the temporary sym link.
+if [ -L dmine ]; then
+    rm dmine
+fi
+
+# Rename back the renamed file/dir, if it exists.
+if [ -f .__temp_dmine__ ] || [ -d .__temp_dmine__ ]; then
+    mv .__temp_dmine__ dmine
 fi
